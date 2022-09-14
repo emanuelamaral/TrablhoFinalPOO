34ec849e4cc6988287844496bea6d8fdca4f2d35
Emanuel Amaral <amoz_c.p.t@hotmail.com>
2022-09-14 17:41:44 -0300

    Projeto de POO

---
+++ Current/Cache/CacheLancamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Current.Cache
+{
+    public static class CacheLancamento
+    {
+        public static Guid? LancamentoID { get; set; }
+        public static string NomeLancamento { get; set; }
+        public static decimal Valor { get; set; }
+        public static string TipoDeLancamento { get; set; }
+        public Usuario usuario { get; set; }
+
+    }
+}
---
+++ Current/Cache/CacheUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Current.Cache
+{
+    public static class CacheUsuario
+    {
+        public static Guid? UsuarioID { get; set; }
+        public static string Nome { get; set; }
+        public static DateTime? DataDeNascimento { get; set; }
+        public static string Celular { get; set; }
+        public static string Cpf { get; set; }
+        public static string Senha { get; set; }
+
+    }
+}
